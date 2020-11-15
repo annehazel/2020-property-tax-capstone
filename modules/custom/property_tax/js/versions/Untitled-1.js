eab19ae354f8baeb989b3743ac4001101b7f3746
Anne Hazel <anne.hazel2@gmail.com>
2020-11-15 13:15:23 -0500

    updated frontpage styling, adjusting margins of node text: margin-left: 8%; margin-top: 3%;

--- modules/custom/property_tax/js/versions/Untitled-1.js
+++ modules/custom/property_tax/js/versions/Untitled-1.js
@@ -43,4 +43,31 @@
 }
 
 
+{
+    cityId: 22002001,
+    cityName: AK: Anchorage,
+    years: {
+        0:1977, 
+        1:1978
+        }
+    series: {
+        0: {
+            genRen: 4956.92,
+            ownSourceRev: 2808.15,
+            ptRev:1472.52
+        },
+        1: {
+            genRen: 5956.92,
+            ownSourceRev: 3808.15,
+            ptRev:2472.52 
+        }
+    }
+
+}
+
+
+
+
+
+
 
